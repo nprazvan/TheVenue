eb8bdd592d1563081c221fd201010310c4850c67
nprazvan <razvan_nicolaie@yahoo.com>
2023-05-26 20:59:45 +0300

    added pricing section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/header_footer/Footer";
 import { Featured } from "./components/featured";
 import VenueInfo from "./components/venueInfo/index";
 import Highlights from "./components/highlights";
+import Pricing from "./components/pricing";
 
 const App = () => {
   return (
@@ -22,6 +23,10 @@ const App = () => {
 
       <>
         <Highlights />
+      </>
+
+      <>
+        <Pricing />
       </>
 
       <div style={{ backgroundColor: "gray", height: "800px" }}></div>
---
+++ src/components/pricing/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import MyButton from "../utils/MyButton";
+import { Zoom } from "react-awesome-reveal";
+
+const Pricing = () => {
+  const priceState = {
+    prices: [30, 40, 50],
+    position: ["Balcony", "Medium", "Star"],
+    desc: ["Balcony tickets", "General access tickets", "VIP tickets"],
+    linkto: ["http://sales.b", "http:sales/m", "http://sales.s"],
+    delay: [500, 0, 500],
+  };
+
+  const showBoxes = () =>
+    priceState.prices.map((box, i) => (
+      <Zoom key={i} className="pricing_item" delay={priceState.delay[i]}>
+        <div className="pricing_inner_wrapper">
+          <div className="pricing_title">
+            <span>{priceState.prices[i]}EUR</span>
+            <span>{priceState.position[i]}</span>
+          </div>
+          <div className="pricing_description">{priceState.desc[i]}</div>
+          <div className="pricing-button">
+            <MyButton
+              text="purchase"
+              size="large"
+              style={{ color: "#ffffff" }}
+              link={priceState.linkto[i]}
+            />
+          </div>
+        </div>
+      </Zoom>
+    ));
+
+  return (
+    <div className="bck_black">
+      <div className="center_wrapper pricing_section">
+        <h2>Pricing</h2>
+        <div className="pricing_wrapper">{showBoxes()}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
